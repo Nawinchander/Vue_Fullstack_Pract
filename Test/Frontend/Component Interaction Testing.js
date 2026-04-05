9381e838eb07cd4cdb33857584c75dbc27dde564
Nawin chander <nawinnapzy289@gmail.com>
2026-04-05 19:45:22 +0530

    add

--- Test/Frontend/Component Interaction Testing.js
+++ Test/Frontend/Component Interaction Testing.js
@@ -1,2 +1,11 @@
 /// Component Interaction Testing
 
+test("button click triggers method", async () => {
+  const wrapper = mount(MongoUser);
+
+  await wrapper.find("button").trigger("click");
+
+  expect(wrapper.vm).toBeDefined();
+});
+
+
