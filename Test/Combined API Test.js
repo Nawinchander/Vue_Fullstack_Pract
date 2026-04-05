46474692771fe0625f63cafa7e1233d511775109
Nawin chander <nawinnapzy289@gmail.com>
2026-04-05 19:30:09 +0530

    add

--- Test/Combined API Test.js
+++ Test/Combined API Test.js
@@ -3,5 +3,22 @@
 // This is what impresses interviewers.
 
 
+describe("Combined API", () => {
+  it("should write to Mongo + MySQL", async () => {
+    const res = await request(app)
+      .post("/api/combined/register")
+      .send({ name: "Nawin", email: "n@mail.com" });
+
+    expect(res.statusCode).toBe(200);
+
+    // Mongo validation
+    expect(res.body.user.email).toBe("n@mail.com");
+
+    // MySQL validation
+    expect(res.body.taskId).toBeDefined();
+  });
+});
+
+
 
 
