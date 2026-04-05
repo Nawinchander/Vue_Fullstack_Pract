a8166565e1bcd40a4c3c6e25730d8f6beeeddb24
Nawin chander <nawinnapzy289@gmail.com>
2026-04-05 19:46:45 +0530

    add

--- Test/Frontend/Router Testing.js
+++ Test/Frontend/Router Testing.js
@@ -1,2 +1,14 @@
 /// Router Testing
 
+import { createRouter, createWebHistory } from "vue-router";
+
+test("router works", () => {
+  const router = createRouter({
+    history: createWebHistory(),
+    routes: [{ path: "/", component: {} }]
+  });
+
+  expect(router).toBeDefined();
+});
+
+
