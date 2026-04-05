3cb3c0a7cd97a300c37e6a775bf5221fe8511095
Nawin chander <nawinnapzy289@gmail.com>
2026-04-05 19:35:37 +0530

    add

--- Test/Frontend/Two-Way Binding.js
+++ Test/Frontend/Two-Way Binding.js
@@ -1,5 +1,17 @@
 /// Two-Way Binding (v-model)
 
 
+test("v-model updates", async () => {
+  const wrapper = mount(MongoUser);
+
+  const input = wrapper.find("input");
+  await input.setValue("Nawin");
+
+  expect(input.element.value).toBe("Nawin");
+});
+
+// Interview highlight:
+
+// “I verify reactivity using v-model binding tests.”
 
 
