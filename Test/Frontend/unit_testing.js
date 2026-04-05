2415489168b82c82e056ee104fe5ccf9430c45f0
Nawin chander <nawinnapzy289@gmail.com>
2026-04-05 19:34:17 +0530

    add

--- Test/Frontend/unit_testing.js
+++ Test/Frontend/unit_testing.js
@@ -1,3 +1,11 @@
 //// Unit Testing (Component Level)
 
+import { mount } from "@vue/test-utils";
+import MongoUser from "../components/MongoUser.vue";
+
+test("renders input", () => {
+  const wrapper = mount(MongoUser);
+  expect(wrapper.find("input").exists()).toBe(true);
+});
+
 
