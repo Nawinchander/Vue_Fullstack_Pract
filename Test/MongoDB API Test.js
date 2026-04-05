93c0788b6cd68e9e7b75771b8da7cbd552d08885
Nawin chander <nawinnapzy289@gmail.com>
2026-04-05 19:28:27 +0530

    add code

--- Test/MongoDB API Test.js
+++ Test/MongoDB API Test.js
@@ -1,2 +1,27 @@
 /// MongoDB API Test (Isolation + DB Mock)
 
+const request = require("supertest");
+const app = require("../index");
+
+describe("Mongo API", () => {
+  it("should create a user", async () => {
+    const res = await request(app)
+      .post("/api/mongo/user")
+      .send({ name: "Test", email: "test@mail.com" });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.name).toBe("Test");
+  });
+
+  it("should fail for invalid input", async () => {
+    const res = await request(app)
+      .post("/api/mongo/user")
+      .send({});
+
+    expect(res.statusCode).toBeGreaterThanOrEqual(400);
+  });
+});
+
+
+
+
